9113abeb0632ea9f58e175c4e7996a4a8f6248d3
Maham-2002 <81827860+Maham-2002@users.noreply.github.com>
2026-05-31 23:20:10 +0500

    Add files via upload

---
+++ Task_3_vulnerable_app.py
@@ -0,0 +1,11 @@
+
+USERNAME = "admin"
+PASSWORD = "admin123"
+
+username = input("Username: ")
+password = input("Password: ")
+
+if username == USERNAME and password == PASSWORD:
+    print("Login Successful")
+else:
+    print("Login Failed")
